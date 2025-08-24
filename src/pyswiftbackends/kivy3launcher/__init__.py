0052f9d1daa523c621236fcedcc087fb402a7a6c
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2025-08-24 13:46:39 +0200

    Add Kivy3Launcher and SDL3 backend modules
    Introduces new backend modules for Kivy3Launcher and SDL3, providing classes for handling dependencies, frameworks, and Swift code integration. These modules extend existing backend functionality to support Kivy 3 and SDL3 frameworks.

---
+++ src/pyswiftbackends/kivy3launcher/__init__.py
@@ -0,0 +1,93 @@
+from backend_tools import CodeBlock, FilePath
+from ..standard_backend import CodePriority
+from ..sdl2 import SDL2Backend
+import pip
+from importlib.util import spec_from_file_location
+
+
+
+pre_main_swift = """
+KivyLauncher.pyswiftImports = [
+    {modules}
+]
+"""
+
+main_swift = """
+let exit_status = KivyLauncher.SDLmain()
+"""
+
+on_exit = """
+exit(exit_status)
+"""
+
+
+class Kivy3LauncherBackend(SDL2Backend):
+    
+    def exclude_dependencies(self) -> list[str]:
+        return [
+            "kivy"
+        ]
+    
+    def packages(self) -> dict:
+        print("adding KivyLauncher packages")
+        return {
+            "KivyLauncher": {
+                "url": "https://github.com/KivySwiftPackages/Kivy3Launcher",
+                "branch": "master"
+            }
+        }
+    
+    def target_dependencies(self, target_type: str):
+        deps = super().target_dependencies(target_type)
+        deps.append(
+            {"package": "Kivy3Launcher", "products": ["KivyLauncher"]}
+        )
+        
+        return deps
+    
+    def wrapper_imports(self, target_type: str) -> list[dict[str, object]]:
+        return [
+            {
+                "libraries": ["KivyLauncher"],
+                "modules": []
+            }
+        ]
+    
+    def copy_to_site_packages(self, site_path: FilePath, platform: str):
+        
+        pips = [
+            "kivy==3.0.0.dev0", "ios", "pyobjus"
+        ]
+        for pip in pips:
+            self.pip_install(
+                pip,
+                "--disable-pip-version-check",
+                f"--platform={platform}",
+                "--only-binary=:all:",
+                "--extra-index-url", "https://pypi.anaconda.org/pyswift/simple", 
+                "--extra-index-url", "https://pypi.anaconda.org/beeware/simple",
+                "--target", str(site_path)
+            )
+    
+    def will_modify_main_swift(self) -> bool:
+        return True
+    
+    def modify_main_swift(self, libraries: list[str], modules: list[str]) -> list["CodeBlock"]:
+        return [
+            CodeBlock(
+                pre_main_swift.format(modules=",\n\t".join(modules)),
+                CodePriority.POST_IMPORTS
+            ),
+            CodeBlock(
+                main_swift,
+                CodePriority.MAIN
+            ),
+            CodeBlock(
+                on_exit,
+                CodePriority.ON_EXIT
+            )
+        ]
+    
+    
+    
+backend = Kivy3LauncherBackend()
